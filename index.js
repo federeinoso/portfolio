962de05f34308445ccd4ce71a62b62add16474a0
Federico <federeinoso01@gmail.com>
2023-03-23 19:24:09 -0300

    portfolio v1.3 (fix night mode)

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const body = document.body;
 const svg = document.querySelectorAll("path");
 const miSVG = document.getElementById("icons");
+const btn = document.getElementById("btn");
+const switcher = document.getElementById("switch");
 
-document.getElementById("switch").addEventListener("change", () => {
+switcher.addEventListener("change", () => {
+  if (switcher.checked) {
+    btn.classList.add("btn-mode");
+    btn.classList.remove("btn-light");
+  } else {
+    btn.classList.add("btn-light");
+    btn.classList.remove("btn-mode");
+  }
   body.classList.toggle("modo-oscuro");
+  btn.classList.add("btn-mode");
   svg.forEach((path) => path.classList.toggle("svg-white"));
   localStorage.setItem("modo-oscuro", body.classList.contains("modo-oscuro"));
 });
